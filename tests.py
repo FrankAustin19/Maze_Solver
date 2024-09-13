c8febbe014c17d63d7c35bef5b9836eedab03877
FrankAustin19 <frank.austin19@gmail.com>
2024-09-13 14:40:29 -0700

    reset cells added

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from maze import Maze
+from graphics import Window
 
 
 class Tests(unittest.TestCase):
@@ -29,6 +30,29 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+    def test_reset_cells_visited(self):
+        num_rows = 12
+        num_cols = 16
+        margin = 50
+        screen_x = 800
+        screen_y = 600
+        cell_size_x = (screen_x - 2 * margin) / num_cols
+        cell_size_y = (screen_y - 2 * margin) / num_rows
+        win = Window(screen_x, screen_y)
+
+        maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
+        for row in maze._cells:
+            for i, cell in enumerate(row):
+                if i % 3 == 0:
+                    cell.visited = True
+
+        maze._reset_cells_visited()
+
+        for row in maze._cells:
+            for cell in row:
+                self.assertFalse(cell.visited, "Cell should not be visited after reset")
+       
 
 
 if __name__ == "__main__":
